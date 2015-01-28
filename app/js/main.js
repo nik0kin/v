5a46618beb1b0bee00c1022bf1cf5ea9cf8fa26e
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2015-01-28 14:32:33 -0600

    correct hookup with mule-sdk

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@
 
 import Vikings from 'js/Vikings'; 
 
-import sdk from 'lib/mule-sdk-js/sdk'
+import sdk from 'lib/mule-sdk-js/mule-sdk';
 
 var vikingsParams = {
   playareaElement: $('#playarea')[0]
@@ -12,12 +12,16 @@ var vikingsParams = {
 var myVikings = new Vikings(vikingsParams);
 
 
-var SDK = sdk('http://zion.tgp.io:313/webservices/');
+var SDK = sdk('http://localhost:3130/webservices/');
 
-SDK.Users.sessionQ()
+var Spinal = SDK.Spinal();
+
+var config = {
+  gameId: '54b489c49cb8bfd8507d4837',
+  userId: '54976c2ecae1efca06ade299'
+};
+
+Spinal.initQ(config)
   .then(function (result) {
     console.log(result);
-  })
-  .catch(function (error) {
-    console.log(error);
   });
