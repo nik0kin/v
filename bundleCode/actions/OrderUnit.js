d3f99eb93882119bee330ad337b5fac3bf8f217f
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2015-02-03 12:57:43 -0600

    add OrderUnit validateQ

--- bundleCode/actions/OrderUnit.js
+++ bundleCode/actions/OrderUnit.js
@@ -1,11 +1,31 @@
 var _ = require('lodash');
 
-var validateCancelOrder = function (M, params) {
+var validateCancelOrder = function (M, unit, orderParams, actionParams) {
+  var orderIndex = orderParams.orderIndex;
+  if (!unit.attributes.orders[orderIndex]) {
+    throw 'invalid CancelOrder orderIndex: ' + orderIndex;
+  }
 
+  // validate no conflicting CancelOrders (no multiple orderIndex or all:true)
+  var count = 0;
+  _.each(actionParams.orders, function (order) {
+    if (order.type === 'CancelOrder' && order.params.orderIndex !== undefined
+        && order.params.orderIndex === orderIndex) {
+      count++;
+    }
+  });
+
+  if (count > 1) {
+    throw 'multiple CancelOrder orderIndex detected: ' + orderIndex;
+  }
 };
 
-var validateMove = function (M, params) {
-
+var validateMove = function (M, unit, orderParams, actionParams) {
+  var spaceId = orderParams.x + ',' + orderParams.y,
+      space = M.getSpace(spaceId);
+    if (!space) {
+      throw 'invalid Move space x,y does not exist: ' + spaceId;
+    }
 };
 
 var validateOrder = {
@@ -29,7 +49,7 @@ var validateQ = function (M, actionOwnerRel, actionParams) {
     if (!order.type || !validateOrder[order.type]) {
       throw 'order: ' + key + ' invalid order type';
     }
-    validateOrder[order.type](M, order);
+    validateOrder[order.type](M, unit, order.params, actionParams);
   });
 };
 
---
+++ bundleCode/test/actions/OrderUnit/validateQSpec.js
@@ -0,0 +1,218 @@
+var should = require('should');
+
+var OrderUnit = require('../../../actions/OrderUnit');
+
+var actionOwner = 'p1';
+
+var validActionParamsCancelOrder = {
+  unitId: 313,
+  orders: [{
+    type: 'CancelOrder',
+    params: {
+      orderIndex: 1
+    }
+  }]
+};
+
+var invalidActionParamsCancelOrder = {
+  unitId: 313,
+  orders: [{
+    type: 'CancelOrder',
+    params: {
+      orderIndex: 1
+    }
+  }, {
+    type: 'CancelOrder',
+    params: {
+      orderIndex: 1
+    }
+  }]
+};
+
+var validActionParamsMove = {
+  unitId: 313,
+  orders: [{
+    type: 'Move',
+    params: {
+      x: 15,
+      y: 17
+    }
+  }]
+};
+
+var validUnitWithNoOrders = {
+  locationId: '3,7',
+  class: 'Viking',
+  unitId: 313,
+  ownerId: 'p1',
+  attributes: {
+    speed: 4,
+    inititative: 50,
+    initBonus: [5, 15],
+    orders: []
+  }
+};
+
+var validUnit = {
+  locationId: '3,7',
+  class: 'Viking',
+  unitId: 313,
+  ownerId: 'p1',
+  attributes: {
+    speed: 4,
+    inititative: 50,
+    initBonus: [5, 15],
+    orders: [{
+      type: 'Move',
+      params: {x: 15, y: 12}
+    }, {
+      type: 'Move',
+      params: {x: 17, y: 13}
+    }]
+  }
+};
+
+var validSpace = {
+  id: '15,17'
+};
+
+describe('Actions:OrderUnit', function () {
+  describe('validateQ', function () {
+    it('should accept valid CancelOrder orders', function (done) {
+      var M = {
+        getPiece: function () {
+          return validUnit;
+        }
+      };
+
+      try {
+        OrderUnit.validateQ(M, actionOwner, validActionParamsCancelOrder);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+
+    it('should accept valid Move orders', function (done) {
+      var M = {
+        getPiece: function () {
+          return validUnit;
+        },
+        getSpace: function () {
+          return validSpace;
+        }
+      };
+
+      try {
+        OrderUnit.validateQ(M, actionOwner, validActionParamsMove);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+
+    it('should reject orders that dont have a valid unitId', function (done) {
+      // in this case, no units exist in M
+      var M = {
+        getPiece: function () {
+          return undefined;
+        }
+      };
+
+      try {
+        OrderUnit.validateQ(M, actionOwner, validActionParamsCancelOrder);
+        done('OrderUnit did not fail')
+      } catch (e) {
+        var index = e.toString().indexOf('unitId not found:');
+        should(index).eql(0);
+        done();
+      }
+    });
+
+    it('should reject orders that are not owned by the player', function (done) {
+      var M = {
+        getPiece: function (id) {
+          if (id === 313) {
+            return {
+              ownerId: 'p2'
+            };
+          }
+          return undefined;
+        }
+      };
+
+      try {
+        OrderUnit.validateQ(M, actionOwner, validActionParamsCancelOrder);
+        done('OrderUnit did not fail')
+      } catch (e) {
+        var index = e.toString().indexOf('unit not owned by player:');
+        should(index).eql(0);
+        done();
+      }
+    });
+
+    it('should reject CancelOrder orders that do not have a valid orderIndex', function (done) {
+      var M = {
+        getPiece: function (id) {
+          if (id === 313) {
+            return validUnitWithNoOrders;
+          }
+          return undefined;
+        }
+      };
+
+      try {
+        OrderUnit.validateQ(M, actionOwner, validActionParamsCancelOrder);
+        done('OrderUnit did not fail')
+      } catch (e) {
+        var index = e.toString().indexOf('invalid CancelOrder orderIndex:');
+        should(index).eql(0);
+        done();
+      }
+    });
+
+    it('should reject CancelOrder orders that have multiple orderIndex occurances', function (done) {
+      var M = {
+        getPiece: function (id) {
+          if (id === 313) {
+            return validUnit;
+          }
+          return undefined;
+        }
+      };
+
+      try {
+        OrderUnit.validateQ(M, actionOwner, invalidActionParamsCancelOrder);
+        done('OrderUnit did not fail')
+      } catch (e) {
+        var index = e.toString().indexOf('multiple CancelOrder orderIndex detected:');
+        should(index).eql(0);
+        done();
+      }
+    });
+
+    it('should reject Move orders that dont reference a valid space', function (done) {
+      // in this case, no valid spaces exist in M
+      var M = {
+        getPiece: function (id) {
+          if (id === 313) {
+            return validUnit;
+          }
+          return undefined;
+        },
+        getSpace: function () {
+          return undefined;
+        }
+      };
+
+      try {
+        OrderUnit.validateQ(M, actionOwner, validActionParamsMove);
+        done('OrderUnit did not fail')
+      } catch (e) {
+        var index = e.toString().indexOf('invalid Move space x,y does not exist:');
+        should(index).eql(0);
+        done();
+      }
+    });
+  });
+});
